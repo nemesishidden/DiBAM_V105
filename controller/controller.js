5adf29c4598395debc67de3daec70e73e7c2ad0d
Juan Pablo Rodriguez <jprodriguez@exe.cl>
2013-07-30 12:04:28 -0400

    m

--- controller/controller.js
+++ controller/controller.js
@@ -60,11 +60,6 @@ var app = {
         scanner.scan(
             function (result) {
                 app.buscarLibro(result.text);
-                //$.mobile.changePage( '#newSolicitudPag', { transition: "slide"} );
-                // alert("Scanner result: \n" +
-                //     "text: " + result.text + "\n" +
-                //     "format: " + result.format + "\n" +
-                //     "cancelled: " + result.cancelled + "\n");
             }, 
             function (error) {
                 alert("Error al escanear el Libro: " + error);
@@ -75,24 +70,31 @@ var app = {
     logear: function(){
         console.log('logear');
         $.ajax({
-            url: 'data/usuario.json',
+            //url: 'data/usuario.json',
+            url: 'http://dibam-sel.opensoft.cl/usuario.asp',
             type: 'GET',
             dataType: 'json',
+            data: JSON.stringify($("#formLogin"). serializeArray()),
             error : function (){ document.title='error'; }, 
             success: function (data) {
                 if(data.success){
-                    var presupuestos = data.model.presupuestos;
-                    var pag = '#inicio';
-                    $.mobile.changePage( pag, { transition: "slide"} );
-                    window.db = baseDatos.abrirBD();
-                    window.db.transaction(function(tx) {
-                            // baseDatos.eliminarTablaPresupuesto(tx);
-                            baseDatos.tablaSolicitudesPorEnviar(tx);
-                            baseDatos.tablaPresupuestos(tx);
-                            baseDatos.verificarPresupuesto(tx, presupuestos);
-                            baseDatos.obtenerPresupuesto(tx);
-                        }, baseDatos.errorTablaSolicitudes, baseDatos.successTablaSolicitudes );
-                }                
+                    if(document.getElementById('username').value == data.model.usuario && document.getElementById('pass').value == data.model.pass){
+                        var presupuestos = data.model.presupuestos;
+                        var pag = '#inicio';
+                        $.mobile.changePage( pag, { transition: "slide"} );
+                        window.db = baseDatos.abrirBD();
+                        window.db.transaction(
+                            function(tx) {
+                                // baseDatos.eliminarTablaPresupuesto(tx);
+                                baseDatos.tablaSolicitudesPorEnviar(tx);
+                                baseDatos.tablaPresupuestos(tx);
+                                baseDatos.verificarPresupuesto(tx, presupuestos);
+                                baseDatos.obtenerPresupuesto(tx);
+                            }, baseDatos.errorTablaSolicitudes, baseDatos.successTablaSolicitudes );
+                    }else{
+                        alert('Usted no se encuentra registrado.');
+                    }       
+                }
             }
         });
     },
@@ -128,7 +130,8 @@ var app = {
 
     buscarLibro: function(codigoIsbn){
         $.ajax({
-            url: 'data/libro.json',
+            //url: 'data/libro.json',
+            url: 'http://dibam-sel.opensoft.cl/libro.asp',
             type: 'POST',
             dataType: 'json',
             error : function (){ document.title='error'; }, 
